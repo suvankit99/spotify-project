65154941ac303cd3dd66c10f5cc8fb7e1f185191
suvankit99 <suvankit2001@gmail.com>
2024-09-04 11:39:49 +0530

    connected to mongodb cluster

--- backend/utils/connectDB.js
+++ backend/utils/connectDB.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 // Replace with your MongoDB connection string
-const mongoURI = 'mongodb://localhost:27017/spotify';
+const mongoURI = "mongodb+srv://suvankits:12345@spotify-cluster.ijriq.mongodb.net/?retryWrites=true&w=majority&appName=spotify-cluster";
 
 // Function to connect to MongoDB
 const connectToDatabase = async () => {
